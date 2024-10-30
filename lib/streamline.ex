4af25efb586dbe632b599fb500db27d08f4fdc52
evwilliams <evanw6@gmail.com>
2024-10-29 23:05:28 -0700

    Add 'map' macro

--- lib/streamline.ex
+++ lib/streamline.ex
@@ -30,4 +30,25 @@ defmodule Streamline do
       end
     end
   end
+
+  @doc """
+  Matches the first argument, `incoming`, against `match` and returns `return`.
+
+  Slightly cleaner to read than `Kernel.then/2` and no anonymous function is created or called.
+
+  ## Examples
+
+        File.read(filename)
+        |> map({:ok, content} ~> content)
+
+        File.read(filename)
+        |> map({:ok, content} ~> String.length(content))
+
+  """
+  defmacro map(incoming, {:~>, _, [match, return]}) do
+    quote do
+      unquote(match) = unquote(incoming)
+      unquote(return)
+    end
+  end
 end
